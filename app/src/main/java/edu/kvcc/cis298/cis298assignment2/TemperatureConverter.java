0e0f3ec46a273f8fb02b053f27d01a69747c60bc
Alyssa Mahler <amstrand21@gmail.com>
2016-10-03 15:55:24 -0400

    Adds initial variables and portrait view layout.

--- app/src/main/java/edu/kvcc/cis298/cis298assignment2/TemperatureConverter.java
+++ app/src/main/java/edu/kvcc/cis298/cis298assignment2/TemperatureConverter.java
@@ -5,7 +5,35 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+// Import widget control groups:
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+
 public class TemperatureConverter extends AppCompatActivity {
+
+    // Variable declarations for widget controls:
+
+    private RadioGroup mFromGroup;
+    private RadioButton mFromCelsius;
+    private RadioButton mFromFahrenheit;
+    private RadioButton mFromKelvin;
+    private RadioButton mFromRankine;
+
+    private RadioGroup mToGroup;
+    private RadioButton mToCelsius;
+    private RadioButton mToFahrenheit;
+    private RadioButton mToKelvin;
+    private RadioButton mToRankine;
+
+    private Button mConvertButton;
+    private TextView mAnswerTextView;
+    private TextView mCalculationTextView;
+
+    // Variable declarations:
+    private double mTemperature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
